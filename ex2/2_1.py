3365e08eb52e796e79ffe9667fe632042ffe0b82
gokullogu <gokulpollachi25@gmail.com>
2021-03-05 21:20:48 +0530

    py

---
+++ ex2/2_1.py
@@ -0,0 +1,14 @@
+nfib=int(input("enter the number"))
+print(nfib)
+if(nfib<0):
+    print("please enter the positive input")
+elif (nfib==1):
+    print("fibonacci sequence upto",nfib,":")
+else:
+    print("fibonacci sequence")
+    count=0;
+    print(count);
+    while(count<10):
+        print(count)
+
+         
---
+++ ex2/2_2.py
@@ -0,0 +1,8 @@
+num=int(input("enter the number :"))
+count=1
+fact=1
+while count<=num:
+    fact*=count
+    count+=1
+print("The factorial of ",num," is ",fact)        
+
