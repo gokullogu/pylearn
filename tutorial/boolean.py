34d5f6b3b64bad04e624ac55231350c0d9f9df80
gokullogu <gokulpollachi25@gmail.com>
2021-03-05 21:39:50 +0530

    py

---
+++ tutorial/boolean.py
@@ -0,0 +1,16 @@
+print(10>9)
+
+a=18
+b=90
+if b>a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+
+#any num,str,list,tuple are true
+#unless num is 0,str,list,tuple are empty
+print(bool(2))    
+
+#returns true if passed obj x is int type
+x=230
+print(isinstance(x,int))
